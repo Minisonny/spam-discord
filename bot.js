c67025ef99a741731f75b85f9ff23bfeaa4f2b05
Duy Anh Vu <duy.a.vu@tuni.fi>
2021-08-16 19:41:48 +0300

    Add handle err

--- bot.js
+++ bot.js
@@ -10,10 +10,25 @@ const EMAIL = "";
 const PASSWORD = "";
 
 // Directory of chromedriver, e.g. "C:/WebDriver/chromedriver.exe"
-const PATH_TO_WEB_DRIVER = "";
+const PATH_TO_WEB_DRIVER = "C:/WebDriver/chromedriver.exe";
 
 // Time out between two messages
-const WAIT_TIME = 1200;
+const WAIT_TIME = 2000;
+
+const typeTextAndRemoveErrorPopUp = async (d, text) => {
+  await d
+    .findElement(By.css("div[class^='textArea']"))
+    .findElement(By.css("div[data-slate-object='block']"))
+    .sendKeys(text, Key.ENTER)
+    .catch(err => err);
+  await d
+    .findElement(By.css("button[class^='primaryButton']"))
+    .then(element => 
+      setTimeout(() => element.click(), 8000),
+      err => err
+    );
+  await new Promise(resolve => setTimeout(resolve, WAIT_TIME));
+};
 
 chrome.setDefaultService(new chrome.ServiceBuilder(PATH_TO_WEB_DRIVER).build());
 (async function example() {
@@ -27,17 +42,9 @@ chrome.setDefaultService(new chrome.ServiceBuilder(PATH_TO_WEB_DRIVER).build());
     await new Promise(resolve => setTimeout(resolve, 5000));
     while (true) {
       for (const [title, lines] of lyrics) {
-        await driver
-          .findElement(By.css("div[class^='textArea']"))
-          .findElement(By.css("div[data-slate-object='block']"))
-          .sendKeys(title.toUpperCase(), Key.ENTER);
-        await new Promise(resolve => setTimeout(resolve, WAIT_TIME));
+        await typeTextAndRemoveErrorPopUp(driver, title.toUpperCase());
         for (const line of lines) {
-          await driver
-            .findElement(By.css("div[class^='textArea']"))
-            .findElement(By.css("div[data-slate-object='block']"))
-            .sendKeys(line, Key.ENTER);
-          await new Promise(resolve => setTimeout(resolve, WAIT_TIME));
+          await typeTextAndRemoveErrorPopUp(line, title.toUpperCase());
         }
       }
     }
